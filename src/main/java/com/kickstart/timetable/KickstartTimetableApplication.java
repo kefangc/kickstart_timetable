4f1b7bd22c952f3ffd9f355185ff62e97952e5c4
clover <zifengz354@gmail.com>
2026-01-14 15:21:51 +0800

    init commit

---
+++ src/main/java/com/kickstart/timetable/KickstartTimetableApplication.java
@@ -0,0 +1,13 @@
+package com.kickstart.timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickstartTimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickstartTimetableApplication.class, args);
+	}
+
+}
